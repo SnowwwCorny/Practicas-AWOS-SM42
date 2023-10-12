20fa3375c84fa0993e69679c2953b1cc7bd835a7
NaiveCannonUT <riquicraft04@gmail.com>
2023-10-11 20:20:21 -0500

    Se le dio estilo a cocteles random

--- src/Pages/CocteleRandom.js
+++ src/Pages/CocteleRandom.js
@@ -32,19 +32,49 @@ function CocteleRandom() {
     return (
         estatus ? (
             <>
-                <h1>Solicitando los datos</h1>
+            <div className="bg-pink-200">
+                <div className="flex justify-center items-center h-screen">
+                    <h1 className="h-auto p-5 text-5xl font-bold">Solicitando los datos...</h1>
+                </div>
+            </div>
+
             </>
         ) : (
 
             datos.map((elcocktail) => (
                 <>
-                    <p className="text-center font-bold text-2xl">{elcocktail.strDrink}</p>
-                    <img src={elcocktail.strDrinkThumb} />
+                    <div className="bg-pink-200">
+                        <h1 className="text-center text-3xl font-bold text-red-500">Nombre del cocktail</h1>
+                        <p className="text-center text-2xl">{elcocktail.strDrink}</p>
+                        <div className="flex justify-center items-center h-screen">
+                            <img className="w-1/2 h-auto p-5" src={elcocktail.strDrinkThumb} />
+                        </div>
+                        <h1 className="text-center text-3xl font-bold text-red-500 p-2">Categoria</h1>
+                        <p className="text-center text-2xl">{elcocktail.strCategory}</p>
+                        <h1 className="text-center text-3xl font-bold text-red-500 p-2">Ingredientes usados</h1>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient1}</p>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient2}</p>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient3}</p>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient4}</p>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient5}</p>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient6}</p>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient7}</p>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient8}</p>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient9}</p>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient10}</p>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient11}</p>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient12}</p>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient13}</p>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient14}</p>
+                        <p className="text-center text-2xl">{elcocktail.strIngredient15}</p>
+
+                    </div>
                 </>
             ))
 
         )
     )
 }
+
 //Ingredientes, categoria y diseño, descripcion en español
 export default CocteleRandom
