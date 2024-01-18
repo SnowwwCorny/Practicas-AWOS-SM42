1e7a36159749086eb08e121ffa564df2b61e12e7
NaiveCannonUT <riquicraft04@gmail.com>
2024-01-17 19:58:30 -0500

    terminado

--- src/components/Componente.js
+++ src/components/Componente.js
@@ -1,21 +1,30 @@
 import React from 'react'
+import User from '../img/User.png'
 
 function Componente() {
     return (
         <>
-                <h1 className="font-bold text-3xl text-center p-5">Inicio de sesion</h1>
-                <img></img>
-            <div className="flex items-center justify-center p-3">
-                <input className="border rounded-lg p-2 w-[30%]" placeholder='Correo'></input>
+        <h1 className="font-bold text-4xl text-center p-5">Inicio de sesion</h1>
+            <div className="flex items-center justify-center p-2">
+                <img className="w-[10%]" src={User}></img>
             </div>
-            <div className="flex items-center justify-center p-3">
-                <input className="border rounded-lg p-2 w-[30%]" placeholder='Contraseña'></input>
-            </div>
-            <p className="text-blue-400 text-center">Recupera tu contraseña</p>
-            <p className="text-blue-400 text-center">¿No tienes cuenta?</p>
+
+                <div className="flex items-center justify-center p-3">
+                    <input className="border rounded-lg p-2 w-[30%]" placeholder='Correo' type='email'></input>
+                </div>
+                <div className="flex items-center justify-center p-3">
+                    <input className="border rounded-lg p-2 w-[30%]" placeholder='Contraseña' type='password'></input>
+                </div>
+
+            <a href='#'>
+                <p className="text-blue-500 text-center hover:text-blue-400">Recupera tu contraseña</p>
+            </a>
+            <a href='#'>
+                <p className="text-blue-500 text-center hover:text-blue-400">¿No tienes cuenta?</p>
+            </a>
 
             <div className="flex items-center justify-center p-3">
-                <button className="bg-indigo-700 border rounded-lg p-2 hover:bg-indigo-600">Login</button>
+                <button className="bg-indigo-700 border rounded-lg p-2 hover:bg-indigo-600 text-white">Login</button>
             </div>
 
         </>
